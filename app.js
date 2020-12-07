fe0cf2a3fa6784e19fcc1b1f30a055b7c0f330b3
Jasmine Smith <Jas@Jasmines-MacBook-Air-2.local>
2020-12-07 15:31:06 -0700

    Some CSS work done, now making the compare page.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 $(() => {
 
-  $('button').on('click', (event) => {
+  $('#openModal').on('click', (event) => {
     const userInput = $("input[type='text']").val();
-
+    $('#modal_screen').css('display', 'block');
 
     $.ajax({
       url: `https://pokeapi.co/api/v2/pokemon/${userInput}/`,
@@ -12,33 +12,34 @@ $(() => {
 
         let img = $('<img id ="picture">');
         img.attr('src', data.sprites.other.dream_world.front_default);
-        img.appendTo('body');
+        img.appendTo('#modal_screen');
 
 
-        $('<h3>').html('Pokemon Name:').appendTo($('body'));
-        $('<p>').html(data.name).appendTo($('body'));
+        $('<h3>').html('Pokemon Name:').appendTo($('#modal_screen'));
+        $('<p>').html(data.name).appendTo($('#modal_screen'));
 
         for(let i = 0; i < data.types.length; i++) {
-        $('<h3>').html('Type:').appendTo($('body'));
-        $('<p>').html(data.types[i].type.name).appendTo($('body'));
+        $('<h3>').html('Type:').appendTo($('#modal_screen'));
+        $('<p>').html(data.types[i].type.name).appendTo($('#modal_screen'));
         }
 
-        $('<h3>').html('Height:').appendTo($('body'));
-        $('<p>').html(data.height).appendTo($('body'));
+        $('<h3>').html('Height:').appendTo($('#modal_screen'));
+        $('<p>').html(data.height).appendTo($('#modal_screen'));
 
-        $('<h3>').html('Weight:').appendTo($('body'));
-        $('<p>').html(data.weight).appendTo($('body'));
+        $('<h3>').html('Weight:').appendTo($('#modal_screen'));
+        $('<p>').html(data.weight).appendTo($('#modal_screen'));
 
-        $('<h3>').html('Pokemon ID (Number in Pokedex)').appendTo($('body'));
-        $('<p>').html(data.id).appendTo($('body'));
+        $('<h3>').html('Pokemon ID (Number in Pokedex)').appendTo($('#modal_screen'));
+        $('<p>').html(data.id).appendTo($('#modal_screen'));
 
-        $('<h3>').html('Featured In These Game Versions:').appendTo($('body'));
+        $('<h3>').html('Featured In These Game Versions:').appendTo($('#modal_screen'));
         for(let i = 0; i < data.game_indices.length; i++) {
-          $('<p>').appendTo($('body')).html(data.game_indices[i].version.name);
-        // $('#games').html(data.game_indices[i].version.name);
+          $('<p>').appendTo($('#modal_screen')).html(data.game_indices[i].version.name);
         };
       });
+        const closeModal = () => {
+        $('#modal_screen').css('display', 'none');
+        };
+        $('#close').on('click', closeModal);
+    });
   });
-
-
-});
