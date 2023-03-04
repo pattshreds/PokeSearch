76b3a869a0d1901efd551a77bf250de843cb9775
Patrick Peralta <patrick.mcguigan5@gmail.com>
2023-03-04 10:15:51 -0700

    Fixed a few issues! title font is working. slider css working better on all browser sizes. fixed issue where API data wasn't resetting on a new search. before each search would display all previous search data plus new search data, now each search displays only current search data.

--- app.js
+++ app.js
@@ -12,34 +12,35 @@ $(() => {
 
         let img = $('<img id ="picture">');
         img.attr('src', data.sprites.other.dream_world.front_default);
-        img.appendTo('#modal_screen');
+        img.appendTo('#modal_screen_data');
 
 
-        $('<h3>').html('Pokemon Name:').appendTo($('#modal_screen'));
-        $('<p>').html(data.name).appendTo($('#modal_screen'));
+        $('<h3>').html('Pokemon Name:').appendTo($('#modal_screen_data'));
+        $('<p>').html(data.name).appendTo($('#modal_screen_data'));
 
         for(let i = 0; i < data.types.length; i++) {
-        $('<h3>').html('Type:').appendTo($('#modal_screen'));
-        $('<p>').html(data.types[i].type.name).appendTo($('#modal_screen'));
+        $('<h3>').html('Type:').appendTo($('#modal_screen_data'));
+        $('<p>').html(data.types[i].type.name).appendTo($('#modal_screen_data'));
         }
 
-        $('<h3>').html('Height:').appendTo($('#modal_screen'));
-        $('<p>').html(data.height).appendTo($('#modal_screen'));
+        $('<h3>').html('Height:').appendTo($('#modal_screen_data'));
+        $('<p>').html(data.height).appendTo($('#modal_screen_data'));
 
-        $('<h3>').html('Weight:').appendTo($('#modal_screen'));
-        $('<p>').html(data.weight).appendTo($('#modal_screen'));
+        $('<h3>').html('Weight:').appendTo($('#modal_screen_data'));
+        $('<p>').html(data.weight).appendTo($('#modal_screen_data'));
 
-        $('<h3>').html('Pokemon ID (Number in Pokedex)').appendTo($('#modal_screen'));
-        $('<p>').html(data.id).appendTo($('#modal_screen'));
+        $('<h3>').html('Pokemon ID (Number in Pokedex)').appendTo($('#modal_screen_data'));
+        $('<p>').html(data.id).appendTo($('#modal_screen_data'));
 
-        $('<h3>').html('Featured In These Game Versions:').appendTo($('#modal_screen'));
+        $('<h3>').html('Featured In These Game Versions:').appendTo($('#modal_screen_data'));
         for(let i = 0; i < data.game_indices.length; i++) {
-          $('<p>').appendTo($('#modal_screen')).html(data.game_indices[i].version.name);
+          $('<p>').appendTo($('#modal_screen_data')).html(data.game_indices[i].version.name);
         };
       });
         const closeModal = () => {
-        $('#modal_screen').css('display', 'none');
-        const userInput = $("input[type='text']").val('');
+          $('#modal_screen_data').empty();  
+          $('#modal_screen').css('display', 'none');
+          userInput = $("input[type='text']").val('');
         };
         $('#close').on('click', closeModal);
     });
