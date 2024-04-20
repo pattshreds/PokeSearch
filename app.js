3ced5d8a8b54ae1d5fe6e06384117a4bd7eccb77
Patrick Mcguigan <pattshreds@patricks-mbp.lan>
2024-04-19 22:43:17 -0600

    fixing some existing styling issues by rebuilding all css. starting with the index page. erased all styling to reset and rework. Finished top of page down to pokedex and just wrapped up the pokedex cover animation.

--- app.js
+++ app.js
@@ -1,46 +1,48 @@
 $(() => {
+    $('#openModal').on('click', (event) => {
+        const userInput = $("input[type='text']").val();
+        $('#modal_screen').css('display', 'block');
+        $.ajax({
+            url: `https://pokeapi.co/api/v2/pokemon/${userInput}/`,
+        }).then((data) => {
+            let img = $('<img id ="picture">');
+            img.attr('src', data.sprites.other.dream_world.front_default);
+            img.appendTo('#modal_screen_data');
 
-  $('#openModal').on('click', (event) => {
-    const userInput = $("input[type='text']").val();
-    $('#modal_screen').css('display', 'block');
+            $('<h3>').html('Pokemon Name:').appendTo($('#modal_screen_data'));
+            $('<p>').html(data.name).appendTo($('#modal_screen_data'));
 
-    $.ajax({
-      url: `https://pokeapi.co/api/v2/pokemon/${userInput}/`,
-    }).then(
-      (data) => {
-        console.log(data);
+            for (let i = 0; i < data.types.length; i++) {
+                $('<h3>').html('Type:').appendTo($('#modal_screen_data'));
+                $('<p>')
+                    .html(data.types[i].type.name)
+                    .appendTo($('#modal_screen_data'));
+            }
 
-        let img = $('<img id ="picture">');
-        img.attr('src', data.sprites.other.dream_world.front_default);
-        img.appendTo('#modal_screen_data');
+            $('<h3>').html('Height:').appendTo($('#modal_screen_data'));
+            $('<p>').html(data.height).appendTo($('#modal_screen_data'));
 
+            $('<h3>').html('Weight:').appendTo($('#modal_screen_data'));
+            $('<p>').html(data.weight).appendTo($('#modal_screen_data'));
 
-        $('<h3>').html('Pokemon Name:').appendTo($('#modal_screen_data'));
-        $('<p>').html(data.name).appendTo($('#modal_screen_data'));
+            $('<h3>')
+                .html('Pokemon ID (Number in Pokedex)')
+                .appendTo($('#modal_screen_data'));
+            $('<p>').html(data.id).appendTo($('#modal_screen_data'));
 
-        for(let i = 0; i < data.types.length; i++) {
-        $('<h3>').html('Type:').appendTo($('#modal_screen_data'));
-        $('<p>').html(data.types[i].type.name).appendTo($('#modal_screen_data'));
-        }
-
-        $('<h3>').html('Height:').appendTo($('#modal_screen_data'));
-        $('<p>').html(data.height).appendTo($('#modal_screen_data'));
-
-        $('<h3>').html('Weight:').appendTo($('#modal_screen_data'));
-        $('<p>').html(data.weight).appendTo($('#modal_screen_data'));
-
-        $('<h3>').html('Pokemon ID (Number in Pokedex)').appendTo($('#modal_screen_data'));
-        $('<p>').html(data.id).appendTo($('#modal_screen_data'));
-
-        $('<h3>').html('Featured In These Game Versions:').appendTo($('#modal_screen_data'));
-        for(let i = 0; i < data.game_indices.length; i++) {
-          $('<p>').appendTo($('#modal_screen_data')).html(data.game_indices[i].version.name);
-        };
-      });
+            $('<h3>')
+                .html('Featured In These Game Versions:')
+                .appendTo($('#modal_screen_data'));
+            for (let i = 0; i < data.game_indices.length; i++) {
+                $('<p>')
+                    .appendTo($('#modal_screen_data'))
+                    .html(data.game_indices[i].version.name);
+            }
+        });
         const closeModal = () => {
-          $('#modal_screen_data').empty();  
-          $('#modal_screen').css('display', 'none');
-          userInput = $("input[type='text']").val('');
+            $('#modal_screen_data').empty();
+            $('#modal_screen').css('display', 'none');
+            userInput = $("input[type='text']").val('');
         };
         $('#close').on('click', closeModal);
     });
