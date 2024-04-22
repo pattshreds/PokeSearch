819d70a1e1015cce195d00983d3604a17a32c3be
Patrick Mcguigan <pattshreds@patricks-mbp.lan>
2024-04-22 17:33:09 -0600

    Styling compare page.Compare container looks shifted off the page to the left. pushing to test on phone.

--- compare.js
+++ compare.js
@@ -1,95 +1,98 @@
 $(() => {
+    $('#compare').on('click', (event) => {
+        const userInput = $("input[type='text']").val();
+        $('#modal_screen').css('display', 'inline');
 
-  $('#compare').on('click', (event) => {
-    const userInput = $("input[type='text']").val();
-    $('#modal_screen').css('display', 'block');
+        $.ajax({
+            url: `https://pokeapi.co/api/v2/pokemon/${userInput}/`,
+        }).then((data) => {
+            console.log(data);
 
-    $.ajax({
-      url: `https://pokeapi.co/api/v2/pokemon/${userInput}/`,
-    }).then(
-      (data) => {
-        console.log(data);
+            let img = $('<img id ="picture">');
+            img.attr('src', data.sprites.other.dream_world.front_default);
+            img.appendTo('#modal_screen_data');
 
-        let img = $('<img id ="picture">');
-        img.attr('src', data.sprites.other.dream_world.front_default);
-        img.appendTo('#modal_screen_data');
+            $('<h3>').html('Pokemon Name:').appendTo($('#modal_screen_data'));
+            $('<p>').html(data.name).appendTo($('#modal_screen_data'));
 
+            for (let i = 0; i < data.types.length; i++) {
+                $('<h3>').html('Type:').appendTo($('#modal_screen_data'));
+                $('<p>')
+                    .html(data.types[i].type.name)
+                    .appendTo($('#modal_screen_data'));
+            }
 
-        $('<h3>').html('Pokemon Name:').appendTo($('#modal_screen_data'));
-        $('<p>').html(data.name).appendTo($('#modal_screen_data'));
+            $('<h3>').html('Height:').appendTo($('#modal_screen_data'));
+            $('<p>').html(data.height).appendTo($('#modal_screen_data'));
 
-        for(let i = 0; i < data.types.length; i++) {
-        $('<h3>').html('Type:').appendTo($('#modal_screen_data'));
-        $('<p>').html(data.types[i].type.name).appendTo($('#modal_screen_data'));
-        }
+            $('<h3>').html('Weight:').appendTo($('#modal_screen_data'));
+            $('<p>').html(data.weight).appendTo($('#modal_screen_data'));
 
-        $('<h3>').html('Height:').appendTo($('#modal_screen_data'));
-        $('<p>').html(data.height).appendTo($('#modal_screen_data'));
-
-        $('<h3>').html('Weight:').appendTo($('#modal_screen_data'));
-        $('<p>').html(data.weight).appendTo($('#modal_screen_data'));
-
-        $('<h3>').html('Base Stats:').appendTo($('#modal_screen_data'));
-        for(let i = 0; i < data.stats.length; i++) {
-          $('<h4>').appendTo($('#modal_screen_data')).html(data.stats[i].stat.name);
-          $('<p>').appendTo($('#modal_screen_data')).html(data.stats[i].base_stat);
-        };
-
-      });
+            $('<h3>').html('Base Stats:').appendTo($('#modal_screen_data'));
+            for (let i = 0; i < data.stats.length; i++) {
+                $('<h4>')
+                    .appendTo($('#modal_screen_data'))
+                    .html(data.stats[i].stat.name);
+                $('<p>')
+                    .appendTo($('#modal_screen_data'))
+                    .html(data.stats[i].base_stat);
+            }
+        });
         const closeModal = () => {
-        $('#modal_screen_data').empty();  
-        $('#modal_screen').css('display', 'none');
-        userInput = $("input[type='text']").val('');
+            $('#modal_screen_data').empty();
+            $('#modal_screen').css('display', 'none');
+            userInput = $("input[type='text']").val('');
         };
-        $('.close').on('click', closeModal);
+        $('#close').on('click', closeModal);
     });
 
-//==================Modal Two====================================
+    //==================Modal Two====================================
 
     $('#compare').on('click', (event) => {
-      const userInput = $("input[id='textbox2']").val();
-      $('#modal_screen2').css('display', 'block');
+        const userInput = $("input[id='textbox2']").val();
+        $('#modal_screen2').css('display', 'block');
 
-      $.ajax({
-        url: `https://pokeapi.co/api/v2/pokemon/${userInput}/`,
-      }).then(
-        (data) => {
-          console.log(data);
+        $.ajax({
+            url: `https://pokeapi.co/api/v2/pokemon/${userInput}/`,
+        }).then((data) => {
+            console.log(data);
 
-          let img = $('<img id ="picture">');
-          img.attr('src', data.sprites.other.dream_world.front_default);
-          img.appendTo('#modal_screen_data2');
+            let img = $('<img id ="picture">');
+            img.attr('src', data.sprites.other.dream_world.front_default);
+            img.appendTo('#modal_screen_data2');
 
+            $('<h3>').html('Pokemon Name:').appendTo($('#modal_screen_data2'));
+            $('<p>').html(data.name).appendTo($('#modal_screen_data2'));
 
-          $('<h3>').html('Pokemon Name:').appendTo($('#modal_screen_data2'));
-          $('<p>').html(data.name).appendTo($('#modal_screen_data2'));
+            for (let i = 0; i < data.types.length; i++) {
+                $('<h3>').html('Type:').appendTo($('#modal_screen_data2'));
+                $('<p>')
+                    .html(data.types[i].type.name)
+                    .appendTo($('#modal_screen_data2'));
+            }
 
-          for(let i = 0; i < data.types.length; i++) {
-          $('<h3>').html('Type:').appendTo($('#modal_screen_data2'));
-          $('<p>').html(data.types[i].type.name).appendTo($('#modal_screen_data2'));
-          }
+            $('<h3>').html('Height:').appendTo($('#modal_screen_data2'));
+            $('<p>').html(data.height).appendTo($('#modal_screen_data2'));
 
-          $('<h3>').html('Height:').appendTo($('#modal_screen_data2'));
-          $('<p>').html(data.height).appendTo($('#modal_screen_data2'));
+            $('<h3>').html('Weight:').appendTo($('#modal_screen_data2'));
+            $('<p>').html(data.weight).appendTo($('#modal_screen_data2'));
 
-          $('<h3>').html('Weight:').appendTo($('#modal_screen_data2'));
-          $('<p>').html(data.weight).appendTo($('#modal_screen_data2'));
-
-          $('<h3>').html('Base Stats:').appendTo($('#modal_screen_data2'));
-          for(let i = 0; i < data.stats.length; i++) {
-            $('<h4>').appendTo($('#modal_screen_data2')).html(data.stats[i].stat.name);
-            $('<p>').appendTo($('#modal_screen_data2')).html(data.stats[i].base_stat);
-          };
-
+            $('<h3>').html('Base Stats:').appendTo($('#modal_screen_data2'));
+            for (let i = 0; i < data.stats.length; i++) {
+                $('<h4>')
+                    .appendTo($('#modal_screen_data2'))
+                    .html(data.stats[i].stat.name);
+                $('<p>')
+                    .appendTo($('#modal_screen_data2'))
+                    .html(data.stats[i].base_stat);
+            }
         });
 
-          const closeModal2 = () => {
-            $('#modal_screen_data2').empty();  
+        const closeModal2 = () => {
+            $('#modal_screen_data2').empty();
             $('#modal_screen2').css('display', 'none');
             userInput = $("input[type='text']").val('');
-          };
-          $('.close2').on('click', closeModal2);
-
-      });
-
+        };
+        $('#close2').on('click', closeModal2);
+    });
 });
