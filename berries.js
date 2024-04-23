49cb3ed07f52f6b562a431096e749d069368af45
Patrick Mcguigan <pattshreds@patricks-mbp.lan>
2024-04-22 18:13:58 -0600

    restyled the about page, mobile styling done.

--- berries.js
+++ berries.js
@@ -1,51 +1,49 @@
 $(() => {
+    $('#berry-btn').on('click', (event) => {
+        const userInput = $("input[type='text']").val();
+        $('#modal_screen').css('display', 'block');
 
-  $('#berryBtn').on('click', (event) => {
-    const userInput = $("input[type='text']").val();
-    $('#modal_screen').css('display', 'block');
+        $.ajax({
+            url: `https://pokeapi.co/api/v2/berry/${userInput}/`,
+        }).then((data) => {
+            console.log(data);
 
-    $.ajax({
-        url: `https://pokeapi.co/api/v2/berry/${userInput}/`,
-      }).then(
-        (data) => {
-          console.log(data);
+            // API has no images for berries that I have found yet.
 
-          // API has no images for berries that I have found yet.
+            // let img = $('<img id ="picture">');
+            // img.attr('src', data.sprites.other.dream_world.front_default);
+            // img.appendTo('#modal_screen_data');
 
-          // let img = $('<img id ="picture">');
-          // img.attr('src', data.sprites.other.dream_world.front_default);
-          // img.appendTo('#modal_screen_data');
+            $('<h3>').html('Berry Name:').appendTo($('#modal_screen_data'));
+            $('<p>').html(data.name).appendTo($('#modal_screen_data'));
 
+            $('<h3>').html('Soil Dryness:').appendTo($('#modal_screen_data'));
+            $('<p>').html(data.soil_dryness).appendTo($('#modal_screen_data'));
 
-          $('<h3>').html('Berry Name:').appendTo($('#modal_screen_data'));
-          $('<p>').html(data.name).appendTo($('#modal_screen_data'));
+            $('<h3>').html('Growth Time:').appendTo($('#modal_screen_data'));
+            $('<p>').html(data.growth_time).appendTo($('#modal_screen_data'));
 
-          $('<h3>').html('Soil Dryness:').appendTo($('#modal_screen_data'));
-          $('<p>').html(data.soil_dryness).appendTo($('#modal_screen_data'));
+            $('<h3>').html('Firmness:').appendTo($('#modal_screen_data'));
+            $('<p>').html(data.firmness.name).appendTo($('#modal_screen_data'));
 
-          $('<h3>').html('Growth Time:').appendTo($('#modal_screen_data'));
-          $('<p>').html(data.growth_time).appendTo($('#modal_screen_data'));
+            $('<h3>').html('Size:').appendTo($('#modal_screen_data'));
+            $('<p>').html(data.size).appendTo($('#modal_screen_data'));
 
+            $('<h3>').html('Smoothness:').appendTo($('#modal_screen_data'));
+            $('<p>').html(data.smoothness).appendTo($('#modal_screen_data'));
 
-          $('<h3>').html('Firmness:').appendTo($('#modal_screen_data'));
-          $('<p>').html(data.firmness.name).appendTo($('#modal_screen_data'));
-
-          $('<h3>').html('Size:').appendTo($('#modal_screen_data'));
-          $('<p>').html(data.size).appendTo($('#modal_screen_data'));
-
-          $('<h3>').html('Smoothness:').appendTo($('#modal_screen_data'));
-          $('<p>').html(data.smoothness).appendTo($('#modal_screen_data'));
-
-          $('<h3>').html('Flavors:').appendTo($('#modal_screen_data'));
-          for(let i = 0; i < data.flavors.length; i++) {
-            $('<p>').appendTo($('#modal_screen_data')).html(data.flavors[i].flavor.name);
-          };
+            $('<h3>').html('Flavors:').appendTo($('#modal_screen_data'));
+            for (let i = 0; i < data.flavors.length; i++) {
+                $('<p>')
+                    .appendTo($('#modal_screen_data'))
+                    .html(data.flavors[i].flavor.name);
+            }
         });
-          const closeModal = () => {
-            $('#modal_screen_data').empty();  
+        const closeModal = () => {
+            $('#modal_screen_data').empty();
             $('#modal_screen').css('display', 'none');
             userInput = $("input[type='text']").val('');
-          };
-          $('.close').on('click', closeModal);
-  });
+        };
+        $('#close').on('click', closeModal);
+    });
 });
